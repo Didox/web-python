683a7259d971b1b91f9b2fd5897be78a908bd770
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-08-07 22:50:44 -0300

    mais itens

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api', views.api, name='api'),
+    path('', views.index),
+    path('api', views.api),
+    path('api2', views.api2),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,36 @@
 from django.http import HttpResponse
-
+from django.http import JsonResponse
+import json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    try:
+        teste = request.GET["teste"]
+    except:
+        teste = "vazio"
+
+    headers = request.headers
+    cookie = request.headers['Cookie']
+    
+    print("===================")
+    print(headers)
+    print("===================")
+    print(cookie)
+    print("===================")
+
+    return HttpResponse(f"Buscando algo da URL do parametro teste = {teste}")
 
 def api(request):
-    teste = request.GET["teste"]
-    return HttpResponse(f"Buscando algo da URL do parametro teste = {teste}")
+    response_data = {}
+    response_data["nome"] = "Danilo"
+    response_data["sobrenome"] = "Aparecido"
+    response_data["site"] = "https://www.torneseumprogramador.com.br"
+
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+def api2(request):
+    response_data = {}
+    response_data["nome"] = "Danilo"
+    response_data["sobrenome"] = "Aparecido"
+    response_data["site"] = "https://www.torneseumprogramador.com.br"
+
+    return JsonResponse(response_data)
